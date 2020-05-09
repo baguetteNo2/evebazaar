0c85008cc161b78878a84f1a6567b064fa658af0
baguetteNo2 <937574328@qq.com>
2020-05-09 16:58:54 +0800

    用户列表模块完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '../components/Login'
 import Home from '../components/Home'
-
+import Welcome from '../components/Welcome'
+import Users from '../components/user/Users'
 Vue.use(Router)
 const router = new Router({
-  routes: [{
+  routes: [ {
     path: '/',
     redirect: '/login'
   },
@@ -15,8 +16,14 @@ const router = new Router({
   },
   {
     path: '/home',
-    component: Home
-  }]
+    component: Home,
+    /* Welcome页面 ，是home页面的子页面，当跳转到home页面的时候会重定向到 Welcome页面 */
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users }
+    ]
+  } ]
 })
 
 /* 挂载路由导航守卫 */
